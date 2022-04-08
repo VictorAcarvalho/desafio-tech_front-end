2efc665c1f2215ffccb6b85bad8b57396dbaa6a1
VictorAcarvalho <carvalho.victorfa@gmail.com>
2022-04-08 15:36:36 -0300

    feat:implementa o componente title

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
-
+import React from 'react';
 import './App.css';
+import Title from './components/title/index';
 
 function App() {
   return (
-    <></>
+    
+      <Title 
+      text="Desafio Calindra"
+      />
+        
+      
+    
     );
 }
 
